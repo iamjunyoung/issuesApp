706bb60e34a4d74998b929a5ca467ce3cffb05bc
junyoung <gdsfav@gmail.com>
2017-10-28 16:51:37 +0900

    Initial Commit

---
+++ issuesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  issuesApp
+//
+//  Created by JUNYOUNG on 2017. 10. 28..
+//  Copyright © 2017년 JUNYOUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
